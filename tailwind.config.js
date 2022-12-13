a15ec1c2c5f6f3e51452d84d6bb6f8f0355ee1b1
sw0xy <aliemreali5@gmail.com>
2022-12-14 00:56:53 +0300

    feat: add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./app/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                pieceBg: "#111221",
+                pieceGray: "#737486",
+                secondary: "#292A3F",
+                border: "#32344D",
+                pieceBlue: "#1D4ED8"
+            }
+        }
+    },
+    plugins: []
+};
